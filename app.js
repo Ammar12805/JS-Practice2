9b896fec114fa62950fd88fc02eb82538cd72f66
Ammar Ahmed <ammarahmed12805@gmail.com>
2024-09-17 18:33:00 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,274 @@
+// chp 9-10
+// Q1
+
+// var pop = prompt("Enter City")
+// var city = "karachi"
+// if (pop === city) {
+// document.write("Welcome to City of lights")    
+// } else {
+//     document.write(pop)
+// }
+
+
+// Q2
+
+// var pop = prompt("Enter your Gender")
+
+// if (pop == "Male") {
+//     document.write("Good Morning Sir")
+// } else if (pop === "Female"){
+//     document.write("Good Morning Mam")
+// } else {
+//     document.write("This Gender Not Exist")
+// }
+
+
+// Q3
+
+// var pop = prompt("Enter Color")
+// var col1 = "Red"
+// var col2 = "Yellow"
+// var col3 = "Green"
+// if (pop === col1) {
+//     document.write("Must Stop")
+// } else if (pop === col2) {
+//     document.write("Ready to Go")
+// }else if (pop === col3) {
+//     document.write("Move on")
+// }else {
+//     document.write("This Color doesn't Exist")
+// }
+
+// Q4
+
+// var pop = prompt("Enter car fuel level in litres")
+// if (pop <= '0.25litres') {
+//   document.write("Please refill your fuel in car")    
+// } else {
+//     document.write("Fuel tank in full")
+// }
+
+// Q5
+
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+
+
+//  var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+//  var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+//  var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+//  if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False");
+// }
+
+//  if("car" < "cat"){
+// alert("car is smaller than cat");
+// }
+
+
+// Q6
+
+// var pop = +prompt("Enter your Subject 1 marks");
+// var pop2 = +prompt("Enter your Subject 2 marks");
+// var pop3 = +prompt("Enter your Subject 3 marks");
+// var totalMarks = 300;
+// var markObatin = pop + pop2 + pop3;
+// var percentage = markObatin/totalMarks*100
+// document.write("<h1>Marks Sheet </h1>  <br> Total Marks: " + totalMarks + " <br> Marks Obtained: " + markObatin + " <br> Percentage: " + percentage + "%" )
+// if (percentage>=80) {
+//     document.write("<br> Grade : A-0ne <br> Remarks : Excellent")
+// } else if (percentage>=70) {
+//     document.write("<br> Grade : A  <br> Remarks : Good")
+// }else if (percentage >=60){
+//     document.write("<br> Grade : B  <br>  Remarks : You Need to Improve")
+// }else {
+//     document.write("<br> Grade : Fail  <br>  Remarks : Sorry")
+// }
+
+
+// Q7
+
+// var pop = +prompt("Guess a Number 1-10");
+// var snum = 7;
+// if (pop === snum) {
+//     document.write("Bingo! Correct Answer")
+// } else if (pop === 6){
+//     document.write("Close enough to the Correct Answer")
+// } else if (pop ===8){
+//     document.write("Close enough to the Correct Answer")
+// } else {
+//     document.write("Try Again")
+// }
+
+// Q8
+
+// var pop = +prompt("Enter Number");
+// if (pop %3 == 0) {
+//     document.write("This Number is divisible by 3")
+// } else {
+//     document.write("This number is not divisible by 3")
+// }
+
+
+// Q9
+
+// var number = +prompt("Enter Number")
+// if (number %2 == 0){
+//     document.write("This is an Even Number")
+// }else{
+//     document.write("This is an odd number")
+// }
+
+// Q10
+
+// var pop = prompt("Enter Temperature")
+// if (pop >=40) {
+//     document.write("It is too Hot today")
+// } else if (pop >=30) {
+//     document.write("The Whether today is Normal")
+// } else if (pop >=20){
+//     document.write("Today's Whether is cold")
+// } else {
+//     document.write("OMG!Today's WEather is so cool")
+// }
+
+// chp 12-13
+
+// Q1
+
+//
+
+// var pop = prompt("Enter Some Thing ")
+// if (pop >= 'A'  &&  pop <= "Z" ) {
+//     document.write("The Input is UpperCase")
+// } else if (pop >= "a"  && pop <= "z") {
+//     document.write("The input is in LowerCase")
+// }else if (pop >= 50){
+//     document.write("The Input is Number")
+// } else {
+//     document.write("This is not a number nor a uppercase or lowercase")
+// }
+
+// Q2
+// var pop = prompt("Enter Integer")
+// var integer = ("a , b")
+
+// if (a > 0 && b > 0 && a > b) {
+//     document.write("The Larger number is " + a)
+// } else if (a > 0 && b > 0 && a > b){
+//     document.write("The Larger number is " + b)
+// } else if (a === b && a > 0 && b > 0 ){
+//     document.write("The Larger number are equal")
+// }else{
+//     document.write("Enter the Integer")
+// }
+
+
+// Q3
+
+// var pop = +prompt("Enter Number")
+// if (pop > 0) {
+//     document.write("The Number is Positive")
+// } else if (pop < 0) {
+//     document.write("The Number is Negative")
+// }else {
+//     document.write("The Number is 0")
+// }
+
+
+// Q4
+
+// var pop = prompt("enter single letter")
+// var vowels = ["a" , "e" , "i" , "o" , "u"] 
+ 
+// if (pop === vowels){
+//     document.write("The Condition is True")
+// } else {
+//     document.write("The Condition is False")
+// }
+
+
+// Q5
+
+// var store = "Thug Life"
+// var pop = prompt("Enter Password")
+// var pop2 = prompt("Re-enter Password")
+// if (pop === store && pop2 === store) {
+//     document.write("Correct! The password you entered matches the original password .")
+// } else if (pop !== store && pop2 !== store) {
+//     document.write("Incorrect Password")
+// }else{
+//     document.write("Enter Password")
+// }
+
+// Q6
+
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+//     greeting = "Good day";
+// }   else{
+//     greeting = "Good evening";
+//     }
+
+// chp 10
+
+// Q1
+
+
+// var pop = prompt("Enter Your City")
+// var city = "Karachi"
+// if (pop === city) {
+//     console.log("The City of Lights")
+// } else {
+//     console.log(pop)
+// }
+
+// Q2
+
+var pop = prompt("Enter a Number");
+var x = 40;
+var y = 50;
+var z ;
+if (x===y){
+    document.write("What is th")
+}
+
+
+// Q3
+// var pop = prompt("Enter city Code")
+// var code = "10010";
+// if (pop === code ) {
+//     alert("Karachi")
+// } else {
+//     alert("Please Write Correct Code")
+// }
+
+
+// Q4
+
